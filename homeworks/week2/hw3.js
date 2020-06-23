8692f67c61ec7cf95dc0cbdc7a472a17bbdab267
Good <good@Goodde-MacBook-Air.local>
2020-06-23 23:41:54 +0800

    hw3 finish

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,14 @@
 function reverse(str) {
-  
+    arr = []
+    arr = str.split("")
+    // console.log(arr);
+    var Newarr = []
+    for (var i=0; i < arr.length; i++){
+        Newarr[i] = arr[arr.length - 1 - i];
+        // console.log(Newarr);
+    };
+    Newarr = Newarr.join("");
+    console.log(Newarr);
 }
 
 reverse('hello');
