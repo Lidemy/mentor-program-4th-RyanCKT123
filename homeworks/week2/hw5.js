b5497f09b973145156b7b061e1e7799938bb485d
Good <good@Goodde-MacBook-Air.local>
2020-06-24 00:22:03 +0800

    hw4 finish

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -2,9 +2,14 @@ function join(arr, concatStr) {
   
 }
 
+var Newstr=''
 function repeat(str, times) {
-  
+    for(var i = 0; i < times; i++){
+        Newstr += str;
+        // console.log(Newstr);
+    };
+    return Newstr;
 }
 
 console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(repeat('abb', 5));
