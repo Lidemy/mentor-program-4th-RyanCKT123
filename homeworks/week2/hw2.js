98aae25ddec97bf13704f6e8c8b2ce031f05ee80
Good <good@Goodde-MacBook-Air.local>
2020-06-23 23:04:08 +0800

    hw2 finish

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
 function capitalize(str) {
+    arr = []
+    arr = str.split("")
+  //   console.log(arr)
+    if(96 < arr[0].charCodeAt() && arr[0].charCodeAt() < 123){
+          arr[0] = arr[0].toUpperCase()}
+    arr = arr.join("");
+    console.log(arr)
+  };
   
-}
-
-console.log(capitalize('hello'));
+  
+  console.log(capitalize('hello'));
+  
