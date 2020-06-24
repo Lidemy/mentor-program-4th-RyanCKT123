a467077f7976ff14af5e1a7922b232f2039928ef
Good <good@Goodde-MacBook-Air.local>
2020-06-24 20:10:56 +0800

    hw5 finished

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,6 +1,13 @@
-// function join(arr, concatStr) {
-  
-// }
+ function join(arr, concatStr) {
+    var Joinstr = ""
+    for(var i = 0; i < arr.length; i++){
+        Joinstr += arr[i];
+        if(i < arr.length - 1 ){
+            Joinstr = Joinstr + concatStr
+        }
+    };
+    return Joinstr;    
+ }
 
 var Newstr=''
 function repeat(str, times) {
@@ -11,5 +18,5 @@ function repeat(str, times) {
     return Newstr;
 }
 
-// console.log(join(['a'], '!'));
+console.log(join(["a"], '!'));
 console.log(repeat('abb', 5));
