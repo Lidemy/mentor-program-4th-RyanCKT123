2e1f673882f20ba0a767ca81fce3f0ba3aaafe17
Good <good@Goodde-MacBook-Air.local>
2020-07-05 21:41:44 +0800

    week3 hw5

---
+++ homeworks/week3/hw5.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const lines = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+/* eslint-disable  no-undef */
+function solve(input) {
+  for (let i = 1; i <= parseInt(lines[0], 10); i += 1) {
+    const Newline = input[i].split(' ');
+    if (BigInt(Newline[2]) === 1 && BigInt(Newline[0]) > BigInt(Newline[1])) {
+      console.log('A');
+    } else if (BigInt(Newline[2]) === 1 && BigInt(Newline[0]) < BigInt(Newline[1])) {
+      console.log('B');
+    } else if (BigInt(Newline[2]) === -1 && BigInt(Newline[0]) > BigInt(Newline[1])) {
+      console.log('B');
+    } else if (BigInt(Newline[2]) === -1 && BigInt(Newline[0]) < BigInt(Newline[1])) {
+      console.log('A');
+    } else { console.log('Draw'); }
+  }
+}
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  solve(lines);
+});
