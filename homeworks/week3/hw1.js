dadc8a06d3fceacd411e8e98e0c37e3b03ed38d1
Good <good@Goodde-MacBook-Air.local>
2020-07-05 20:52:05 +0800

    week3 hw1

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+const lines = [];
+function solve(input) {
+  for (let j = 1; j <= input; j += 1) {
+    let tem = '';
+    for (let i = 1; i <= j; i += 1) {
+      tem += '*';
+    }
+    console.log(tem);
+  }
+}
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  solve(lines);
+});
