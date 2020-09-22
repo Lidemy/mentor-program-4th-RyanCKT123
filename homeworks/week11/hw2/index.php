4523df9a352a34d67844c258c0b396cc6b7accd3
Good <good@Goodde-MacBook-Air.local>
2020-09-22 22:38:46 +0800

    blog mock-up finished

---
+++ homeworks/week11/hw2/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("conn.php");
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="blog-login">
+
+    </div>
+</body>
+</html>
